e6b03e6ba0f96d3188bd340be72781c7a6ef75b3
Matthew Malone <31638037+mattm-malone@users.noreply.github.com>
2021-06-15 14:24:00 -0400

    chore: refactor with promise chaining

--- src/index.ts
+++ src/index.ts
@@ -19,9 +19,11 @@ export interface RetryOptions {
   when?: (result: any) => boolean;
 }
 
-function sleep(ms: number) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-}
+const checkCondition = (result: any, when: ((result: any) => boolean) | undefined) => {
+  if (result instanceof Error) return result;
+  else if (when && when(result)) throw new Error('Failed');
+  else return result;
+};
 
 export default function withRetries<T>(
   func: (...params: any[]) => T,
@@ -35,15 +37,38 @@ export default function withRetries<T>(
   }
 ): (...args: any[]) => Promise<T | undefined> {
   return async function retry(...args: any[]): Promise<T | undefined> {
-    for (let i = 0; i < maxAttempts; i++) {
-      try {
-        const funcResult = func.apply(scope, args);
-        if (when && when(funcResult)) throw new Error('Failed');
-        return funcResult;
-      } catch (err) {
-        if (i < maxAttempts - 1) await sleep(delay);
-        else throw err;
-      }
-    }
+    return new Promise((resolve, reject) => {
+      let attempt = 1;
+      const handleErr = (err: any) => {
+        if (attempt < maxAttempts) {
+          attempt += 1;
+          setTimeout(applied, delay);
+        } else reject(err);
+      };
+
+      const applied = () =>
+        new Promise((resolve, reject) => {
+          try {
+            const result = func.apply(scope, args);
+            resolve(result);
+          } catch (err) {
+            reject(err);
+          }
+        })
+          .then((res) => checkCondition(res, when))
+          .then(resolve)
+          .catch(handleErr);
+      return applied();
+    });
+    // for (let i = 0; i < maxAttempts; i++) {
+    //   try {
+    //     const funcResult = func.apply(scope, args);
+    //     if (when && when(funcResult)) throw new Error('Failed');
+    //     return funcResult;
+    //   } catch (err) {
+    //     if (i < maxAttempts - 1) await sleep(delay);
+    //     else throw err;
+    //   }
+    // }
   };
 }
