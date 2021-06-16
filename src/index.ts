abdefc282d8414b86fe530dc0c8fe06044b900a9
Matthew Malone <31638037+mattm-malone@users.noreply.github.com>
2021-06-16 15:20:40 -0400

    feat: Add proper JSDoc

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
+/**
+ * Options used to configure the retry logic.
+ */
 interface RetryOptions {
   /**
    * The value to use as `this` when calling a given function.
    */
-  scope?: unknown;
+  scope?: any;
   /**
    * How many times to attempt function upon failure.
    */
@@ -12,10 +15,10 @@ interface RetryOptions {
    */
   delay?: number;
   /**
-   * Function that returns true or false based on the result of the given function.
-   * Indicates whether to retry or not
    * If function returns true, it will retry again (after the specified delay).
    * else, return result.
+   * Function that returns true or false based on the result of the given function.
+   * Indicates whether to retry or not
    */
   when?: (result: any) => boolean;
 }
@@ -24,10 +27,24 @@ function sleep(ms: number) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
+/**
+ * Creates a new async function that, when invoked, will invoke the given func.
+ * If the given func throws an error or rejects, with-retries will attempt to invoke the function again.
+ *
+ * @param {function(...args:*):boolean} func - The function to invoke.
+ * @param {Object} [options] - Options used to configure how `func` is retried and invoked.
+ * @param {object} [options.scope] - The value to use as `this` when calling `func`.
+ * @param {number} [options.maxAttempts=3] - How many times to attempt function upon failure.
+ * @param {number} [options.delay=500] - Time in milliseconds to delay each re-invocation.
+ * @param {function(result:*):boolean} [options.when] - Function that determines whether to retry or not.
+ *
+ * @returns Returns async function that, when invoked, will invoke the given func.
+ */
 export default function withRetries<T>(
-  func: (...params: any[]) => Promise<T> | T,
-  { delay = 500, maxAttempts = 3, scope, when }: RetryOptions = {}
+  func: (...args: any[]) => Promise<T> | T,
+  options: RetryOptions = {}
 ): (...args: any[]) => Promise<T | undefined> {
+  const { delay = 500, maxAttempts = 3, scope, when } = options;
   return async function retry(...args: any[]): Promise<T | undefined> {
     for (let attempt = 1; attempt <= maxAttempts; attempt++) {
       try {
